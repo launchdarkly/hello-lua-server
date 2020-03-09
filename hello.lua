ecac447c9d78c8487df2bd3f2fc031ead709d26b
hroederld <hroeder@launchdarkly.com>
2020-03-09 14:15:34 -0700

    [ch68370] basic hello app (#1)

---
+++ hello.lua
@@ -0,0 +1,20 @@
+local ld = require("launchdarkly")
+
+local YOUR_SDK_KEY = "<put your SDK key here>"
+local YOUR_FEATURE_KEY = "<put your feature key here>"
+
+local config = {
+    key = YOUR_SDK_KEY
+}
+
+local user = ld.makeUser({
+    key = "abc"
+})
+
+local client = ld.clientInit(config, 1000)
+
+if client.boolVariation(user, YOUR_FEATURE_KEY, false) then
+    print "feature is enabled"
+else
+    print "feature is disabled"
+end
