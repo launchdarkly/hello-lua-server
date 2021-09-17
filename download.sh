84957796f9641c7e02f064253331c85bc29bc651
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2021-09-17 09:08:00 -0700

    ch122170 Move download/extract of the SDK to a bash function.

--- download.sh
+++ download.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 set -e
 
+download_sdk() {
+    echo "downloading $1 c-server-sdk shared library"
+    rm -rf $2 'include' 'lib'
+    curl https://api.github.com/repos/launchdarkly/c-server-sdk/releases/latest | \
+    grep browser_download_url | \
+    grep $2 | \
+    cut -d '"' -f 4 | \
+    xargs curl -s -o $2 -L
+    tar -xvf $2
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo 'downloading linux c-server-sdk shared library'
-    rm -rf 'linux-gcc-64bit-dynamic.tar' 'include' 'lib'
-    curl https://api.github.com/repos/launchdarkly/c-server-sdk/releases/latest | \
-    grep browser_download_url | \
-    grep linux-gcc-64bit-dynamic.tar | \
-    cut -d '"' -f 4 | \
-    xargs curl -s -o 'linux-gcc-64bit-dynamic.tar' -L
-    tar -xvf 'linux-gcc-64bit-dynamic.tar'
+    download_sdk linux linux-gcc-64bit-dynamic.tar
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo 'downloading osx c-server-sdk shared library'
-    rm -rf 'osx-clang-64bit-dynamic.tar' 'include' 'lib'
-    curl https://api.github.com/repos/launchdarkly/c-server-sdk/releases/latest | \
-    grep browser_download_url | \
-    grep osx-clang-64bit-dynamic.tar | \
-    cut -d '"' -f 4 | \
-    xargs curl -s -o 'osx-clang-64bit-dynamic.tar' -L
-    tar -xvf 'osx-clang-64bit-dynamic.tar'
+    download_sdk osx osx-clang-64bit-dynamic.tar
 else
     echo "unknown platform"
     exit 1
