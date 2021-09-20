37ee0aeccac031e29b92e159c0ab9f65593b047b
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2021-09-20 10:26:50 -0700

    ch122170 Download the latest c-server-sdk release instead of a fixed version. (#9)
    * ch122170 Download the latest c-server-sdk release instead of a fixed version.
    
    * ch122170 Update circle CI config to use specific lua version.
    
    * ch122170 Correct issue with brew command.
    
    * ch122170 Run with lua5.1
    
    * ch122170 Move download/extract of the SDK to a bash function.
    
    * ch122170 Download the latest version by semantic version.
    
    * ch122170 Make clear that the download script is not suitable for production.

--- download.sh
+++ download.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
 set -e
 
+# Download the c-server-sdk.
+#
+# $1: The platform the SDK is being downloaded for.
+# $2: The expected name of the SDK bundle file.
+# $3: Optional major version. Downloads the latest minor/patch unless specified.
+# $4: Optional minor version. Downloads the latest patch unless specified.
+# $5: Optional patch version.
+# Examples:
+#
+# Download the latest osx version.
+# download_sdk osx osx-clang-64bit-dynamic.tar
+#
+# Download the latest 2.x version for osx.
+# download_sdk osx osx-clang-64bit-dynamic.tar 2
+#
+# Download a specific version of the SDK.
+# download_sdk osx osx-clang-64bit-dynamic.tar 2 1 1
+download_sdk() {
+    echo "downloading $1 c-server-sdk shared library"
+    rm -rf $2 'include' 'lib'
+    major=${3:-[0-9]+}
+    minor=${4:-[0-9]+}
+    patch=${5:-[0-9]+}
+    #Get the list of releases.
+    #Trim that to a list of version numbers one on each line.
+    #Exclude any pre-release versions.
+    #Sort the versions from highest to lowest
+    #Take the first one.
+	curl https://api.github.com/repos/launchdarkly/c-server-sdk/releases/tags/$(
+		curl https://api.github.com/repos/launchdarkly/c-server-sdk/releases | \
+        grep tag_name | \
+        cut -d '"' -f 4 | \
+        grep -E "$major\.$minor\.$patch$" | \
+        sort -V -r | \
+        head -n 1) | \
+    grep browser_download_url | \
+    grep $2 | \
+    cut -d '"' -f 4 | \
+    xargs curl -s -o $2 -L
+    tar -xvf $2
+}
+
+# This will download the latest version of the SDK.
+# For a production application it would be preferable to download a specific
+# version. 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo 'downloading linux c-server-sdk shared library'
-    rm -rf 'linux-gcc-64bit-dynamic.tar' 'include' 'lib'
-    curl -s -o 'linux-gcc-64bit-dynamic.tar' -L 'https://github.com/launchdarkly/c-server-sdk/releases/download/2.2.1/linux-gcc-64bit-dynamic.tar'
-    tar -xvf 'linux-gcc-64bit-dynamic.tar'
+    download_sdk linux linux-gcc-64bit-dynamic.tar
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo 'downloading osx c-server-sdk shared library'
-    rm -rf 'osx-clang-64bit-dynamic.tar' 'include' 'lib'
-    curl -s -o "osx-clang-64bit-dynamic.tar" -L "https://github.com/launchdarkly/c-server-sdk/releases/download/2.2.1/osx-clang-64bit-dynamic.tar"
-    tar -xvf 'osx-clang-64bit-dynamic.tar'
+    download_sdk osx osx-clang-64bit-dynamic.tar
 else
     echo "unknown platform"
     exit 1
