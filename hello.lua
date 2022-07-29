932f38b618e13d1e4074b91c01c0cd15e3cd711f
Ember Stevens <79482775+ember-stevens@users.noreply.github.com>
2022-07-29 14:27:35 -0700

    feat: [sc-154155] update Lua SDK hello app readme and comments to standard design (#11)
    * Updates to match spec
    
    * Update README.md
    
    Co-authored-by: Molly <molly.jones@launchdarkly.com>
    
    * Update print statement
    
    Co-authored-by: Molly <molly.jones@launchdarkly.com>
    Co-authored-by: Casey Waldren <cwaldren@launchdarkly.com>

--- hello.lua
+++ hello.lua
@@ -1,7 +1,9 @@
 local ld = require("launchdarkly_server_sdk")
 
-local YOUR_SDK_KEY = "<put your SDK key here>"
-local YOUR_FEATURE_KEY = "<put your feature key here>"
+-- Set YOUR_SDK_KEY to your LaunchDarkly SDK key.
+local YOUR_SDK_KEY = ""
+-- Set YOUR_FEATURE_KEY to the feature flag key you want to evaluate.
+local YOUR_FEATURE_KEY = "my-boolean-flag"
 
 local config = {
     key = YOUR_SDK_KEY
@@ -10,11 +12,9 @@ local config = {
 local client = ld.clientInit(config, 1000)
 
 local user = ld.makeUser({
-    key = "abc"
+    key = "example-user-key",
+    name = "Sandy"
 })
 
-if client:boolVariation(user, YOUR_FEATURE_KEY, false) then
-    print "feature is enabled"
-else
-    print "feature is disabled"
-end
+local value = client:boolVariation(user, YOUR_FEATURE_KEY, false)
+print("feature flag "..YOUR_FEATURE_KEY.." is "..tostring(value).." for this user")
